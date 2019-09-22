e8c4febf1cf027e23bbf1d8030e9f5cb5e63d2ed
johanaanez <johana.anez@gmail.com>
2019-09-21 21:40:09 -0300

    add app.js and app.py

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+const PORT = 3000
+const TIMEOUT = 5000
+const ID = Math.floor(Math.random()+100)
+
+app.get('/', function (req, res) {
+  res.status(200).send('node ping\n'+ ID);
+});
+
+app.get('/timeout', function (req, res) {
+  setTimeout(() => { 
+    res.status(200).send('node - timeout \n'+ ID);
+  },TIMEOUT);
+});
+
+app.listen(PORT, function () {
+  ID + console.log(' - Escuchando en el puerto', PORT);
+});
