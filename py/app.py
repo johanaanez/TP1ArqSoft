b157013fa43061b8808027a95534227ed46f7760
johanaanez <johana.anez@gmail.com>
2019-09-29 16:27:34 -0300

    add heavy endpoint in python

--- py/app.py
+++ py/app.py
@@ -1,5 +1,6 @@
 import time
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,8 +10,19 @@ def ping():
 
 @app.route("/timeout")
 def timeout():
-	time.sleep(5);
+	time.sleep(5)
 	return "python - timeout"
+
+@app.route("/heavy")
+def heavy():
+  start = datetime.now()
+  startToMillisecond = start.timestamp() * 1000
+  times = 99999
+  for i in range(times):
+    now = datetime.now()
+    nowToMillisecond = now.timestamp() * 1000
+    if (nowToMillisecond - startToMillisecond >= 5000):
+      break
 
 if __name__ == "__main__":
 	app.run()
